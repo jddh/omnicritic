49c097946cd62b3735a511f5fc051b025d487fd5
Jedd <jedd@autoboxmedia.com>
2023-04-03 11:32:16 -0700

    complete module folders, metacritic loop

--- colonel.js
+++ colonel.js
@@ -1,13 +1,20 @@
 import * as db from '#models/db/db';
-import tetch from './tetch.js';
-import * as unogs from './unogs.js';
+import tetch from '#models/tetch';
+import * as unogs from '#models/unogs';
+import * as mcrunch from '#models/scrape/mcrunch';
 
 await db.init();
 
 // await db.testInsert();
 
-let t = await unogs.getTitles();
+// let t = await unogs.getTitles();
 
-await db.saveTitles(t.results);
+// await db.saveTitlesFromUnogs(t.results);
+await unogs.fetchTitlesLoop();
+
+await mcrunch.addRatingLoop(10);
+
+// const q = await mcrunch.getRatingFromTitle('Blade Runner');
+// console.log(q);
 
 db.quit();
