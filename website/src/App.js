9833e59f7011070c22651098d1ae2affd0df57ec
Jedd <jedd@autoboxmedia.com>
2023-04-25 11:55:45 -0700

    react: refactor & reducer for fetches

--- website/src/App.js
+++ website/src/App.js
@@ -6,31 +6,69 @@ import SearchBox from './SearchBox';
 import FilterButtons from './FilterButtons'
 import SortingColumn from './SortingColumn';
 
-// import db from './lib/db/db.js';
+function dispatchApi(state, action) {
+	switch(action.type) {
+		case 'API_FETCH_INIT':
+			return {
+				...state,
+				isLoading: true,
+				isError: false
+			}
+		case 'API_FETCH_COMPLETE':
+			return {
+				data: action.payload,
+				isLoading: false,
+				isError: false
+			}
+		case 'API_FETCH_ERROR':
+			return {
+				...state,
+				isLoading: false,
+				isError: true
+			}
+	}
+}
+
+async function fetchFromApi(endpoint) {
+	const ts = await fetch('http://localhost:4000/' + endpoint);
+	const tsj = await ts.json();
+	const data = tsj.map(d => {
+		if (d.ratings?.metacritic?.rating == 'tbd')
+			delete d.ratings.metacritic.rating;
+		return d;
+	});
+
+	return data;
+}
 
 function App() {
 
-	const [data, setData] = React.useState([]);
-	const [masterData, setMasterData] = React.useState([]);
+	const [dataView, setDataView] = React.useState([]);
 	const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('searchTerm') || '');
 	const inputTimer = React.useRef(null);
 
 	React.useEffect(() => {
 		(async function() {
-			const ts = await fetchFromApi('rated');
-
-			setMasterData(ts);
-			setData(ts);
-
-			// if (searchTerm) handleSearch({target:{value:searchTerm}});
+			dispatch({type:'API_FETCH_INIT'});
+			fetchFromApi('rated')
+			.then((ts) => {
+				dispatch({type:'API_FETCH_COMPLETE', payload:ts});
+			})
+			.catch(() => {
+				dispatch({type:'API_FETCH_ERROR'})
+			})
 		})()
 	}, []);
+
+	const [apiFeed, dispatch] = React.useReducer(dispatchApi, { data: [], isLoading: false, isError: false });
+
+	React.useEffect(() => {
+		setDataView(apiFeed.data);
+	}, [apiFeed.data])
 
 	React.useEffect(() => {
 		localStorage.setItem('searchTerm',searchTerm);
 	},[searchTerm])
-
-	const dataForRender = data.filter(i => i.title.match(new RegExp(searchTerm, 'gi')));
 
 	function handleSearch(event) {
 		const term = event.target.value;
@@ -50,30 +88,14 @@ function App() {
 		const filter = event.target.id;
 		const apiUrl = filter.replace(/filter-(.*?)/gi, '$1');
 		const ts = await fetchFromApi(apiUrl);
-		setMasterData(ts);
-		setData(ts);
-	}
-
-	function sortByScore() {
-		console.log('sorting...');
-		let sortedData = [...data];
-		sortedData = sortedData.sort((a, b) => {
-			const ar = parseInt(a.ratings?.metacritic.rating);
-			const br = parseInt(b.ratings?.metacritic.rating);
-			
-			if (ar > br || ar && !br) return -1;
-			if (ar < br || !ar && br) return 1;
-			return 0;
-		})
-
-		setData(sortedData);
+		setDataView(ts);
 	}
 
 	const [activeSort, setActiveSort] = React.useState(false);
 
 	function sorter(dataSource, direction = 'desc') {
 		console.log('sorting ' + dataSource + ' ' + direction);
-		let sortedData = [...data];
+		let sortedData = [...dataView];
 		sortedData = sortedData.sort((a, b) => {
 			const ar = parseInt(a.ratings[dataSource]?.rating);
 			const br = parseInt(b.ratings[dataSource]?.rating);
@@ -90,25 +112,18 @@ function App() {
 		})
 
 		setActiveSort(dataSource);
-		setData(sortedData);
+		setDataView(sortedData);
 	}
 
-	async function fetchFromApi(endpoint) {
-		const ts = await fetch('http://localhost:4000/' + endpoint);
-		const tsj = await ts.json();
-		const data = tsj.map(d => {
-			if (d.ratings?.metacritic?.rating == 'tbd')
-				delete d.ratings.metacritic.rating;
-			return d;
-		});
-
-		return data;
-	}
-
-	if (!data) return <div>Loading...</div>;
+	const dataForRender = dataView.filter(i => i.title.match(new RegExp(searchTerm, 'gi')));
 
 	return (
+
 		<div className="App">
+			{apiFeed.isLoading && <div>Loading...</div>}
+
+			{apiFeed.isError && <div>Data fetch error!</div>}
+
 			<main>
 				<h2>Rated titles</h2>
 			
